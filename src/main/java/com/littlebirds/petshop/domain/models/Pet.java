c13d8565960bcc32557184d57f3848fb18c9f4a0
juarez <juarezgcjr@gmail.com>
2025-10-24 13:23:28 -0300

    feat: add new props to `Pet` and add edit info method

--- src/main/java/com/littlebirds/petshop/domain/models/Pet.java
+++ src/main/java/com/littlebirds/petshop/domain/models/Pet.java
@@ -1,9 +1,12 @@
 package com.littlebirds.petshop.domain.models;
 
+import com.littlebirds.petshop.domain.dtos.pet.EditPetInfoDto;
 import com.littlebirds.petshop.domain.dtos.pet.PetRegisterDto;
 import com.littlebirds.petshop.domain.enums.PetType;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pets")
@@ -29,14 +32,41 @@ public class Pet {
     @Enumerated(EnumType.STRING)
     private PetType petType;
 
-    @Column
+    @Column(nullable = false)
     private String race;
 
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private LocalDateTime born;
 
     public Pet(PetRegisterDto pet,Client client) {
         this.client = client;
         this.name = pet.name();
         this.petType = pet.petType();
         this.race = pet.race();
+        this.color = pet.color();
+        this.born = pet.born();
+    }
+
+    public void editPetInfo(EditPetInfoDto petInfoDto)
+    {
+        if(petInfoDto.name()!=null)
+        {
+            this.name = petInfoDto.name();
+        }
+        if(petInfoDto.race()!=null)
+        {
+            this.race = petInfoDto.race();
+        }
+        if(petInfoDto.color()!=null)
+        {
+            this.color = petInfoDto.color();
+        }
+        if(petInfoDto.born()!=null)
+        {
+            this.born = petInfoDto.born();
+        }
     }
 }
